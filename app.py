64949b46d2efebfee1b4abd5461dc64db4821c4e
saeedgoraya987 <saeedgoraya987@gmail.com>
2025-08-28 15:40:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # ✅ Per-key rate limit setup
-KEY_LIMIT = 1000
+KEY_LIMIT = 150
 token_tracker = defaultdict(lambda: [0, time.time()])  # token: [count, last_reset_time]
 
 def get_today_midnight_timestamp():
