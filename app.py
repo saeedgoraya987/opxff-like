7fc89c526ac88880c80b7ecb92de55fac122a1a6
saeedgoraya987 <saeedgoraya987@gmail.com>
2025-08-31 17:03:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ def load_tokens(server_name):
         elif server_name == "PK":
             with open("token_pk.json", "r") as f:
                 tokens = json.load(f)
+        elif server_name == "VN":
+            with open("token_vn.json", "r") as f:
+                tokens = json.load(f)
         else:
             with open("token_bd.json", "r") as f:
                 tokens = json.load(f)
